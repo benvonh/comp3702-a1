c46f350d0b2fc296f17aef1e1ec988b57ffd3bd1
benvonh <benjaminvonsnarski@gmail.com>
2022-08-20 00:32:47 +1000

    Accounts for node expansions that do not result in more states

--- solution.py
+++ solution.py
@@ -24,7 +24,7 @@ class Solver:
         self.environment = environment
         self.loop_counter = loop_counter
         self._queue = [] # array of (running_cost, [action_history], new_state) tuple
-        self._visited_states = set()
+        self._visited_states = {}
 
 
     def solve_ucs(self):
@@ -32,15 +32,14 @@ class Solver:
         Find a path which solves the environment using Uniform Cost Search (UCS).
         :return: path (list of actions, where each action is an element of ROBOT_ACTIONS)
         """
-        self._visited_states.add(self.environment.get_init_state())
+        self._visited_states = { self.environment.get_init_state(): 0 }
         lowest_cost_action = (0, 0, self.environment.get_init_state())
         self.expand_node(0, [], self.environment.get_init_state())
-        self.loop_counter.inc()
 
         while not self.environment.is_solved(lowest_cost_action[2]):
             lowest_cost_action = hq.heappop(self._queue)
-            self.expand_node(*lowest_cost_action)
-            self.loop_counter.inc()
+            if self.expand_node(*lowest_cost_action):
+                self.loop_counter.inc()
 
         return lowest_cost_action[1]
 
@@ -51,8 +50,7 @@ class Solver:
         :return: path (list of actions, where each action is an element of ROBOT_ACTIONS)
         """
         self._queue = [] # array of (running_cost, [action_history], new_state) tuple
-        self._visited_states = set()
-        self._visited_states.add(self.environment.get_init_state())
+        self._visited_states = { self.environment.get_init_state(): 0 }
         lowest_cost_action = (0, 0, self.environment.get_init_state())
         self.expand_node(0, [], self.environment.get_init_state())
         self.loop_counter.inc()
@@ -65,13 +63,19 @@ class Solver:
         return lowest_cost_action[1]
 
 
-    def expand_node(self, running_cost: float, action_history: list, prev_state: State):
+    def expand_node(self, running_cost: float, action_history: list, prev_state: State) -> bool:
         """
+        Expands the node from prev_state by iterating through all possible actions and adding the tuple form to _queue.
         """
+        new = False
         for action in ROBOT_ACTIONS:
             success, cost, state = self.environment.perform_action(prev_state, action)
-            if success and state not in self._visited_states:
-                history = action_history.copy()
-                history.append(action)
-                self._queue.append((cost + running_cost, history, state))
-                self._visited_states.add(state)
+            if success:
+                if state not in self._visited_states or self._visited_states[state] > cost + running_cost:
+                    history = action_history.copy()
+                    history.append(action)
+                    self._queue.append((cost + running_cost, history, state))
+                    self._visited_states[state] = cost + running_cost
+                    new = True
+
+        return new
